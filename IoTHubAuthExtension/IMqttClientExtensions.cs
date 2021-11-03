85540c52c1ccf4ea31de6c56d25ee9c154deb0b8
rido-min <rido-min@users.noreply.github.com>
2021-11-02 18:47:39 -0700

    first rel

---
+++ IoTHubAuthExtension/IMqttClientExtensions.cs
@@ -0,0 +1,113 @@
+﻿using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Connecting;
+using MQTTnet.Client.Options;
+using MQTTnet.Diagnostics;
+using MQTTnet.Diagnostics.Logger;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System.Threading.Tasks;
+using MQTTnet.Extensions.IoTHubAuth;
+
+namespace MQTTnet
+{
+    public static class IMqttClientExtensions
+    {
+        public static async Task<MqttClientConnectResult> ConnectWithSasAsync(this IMqttClient mqttClient, string hostName, string deviceId, string sasKey, string modelId = "", int minutes = 60)
+        {
+            (string username, byte[] password) = SasAuth.GenerateHubSasCredentials(hostName, deviceId, sasKey, modelId, minutes);
+            return await mqttClient.ConnectAsync(new MqttClientOptionsBuilder()
+                 .WithClientId(deviceId)
+                 .WithTcpServer(hostName, 8883)
+                 .WithCredentials(username, password)
+                 .WithTls(new MqttClientOptionsBuilderTlsParameters
+                 {
+                     UseTls = true,
+                     SslProtocol = SslProtocols.Tls12
+                 })
+                 .Build());
+        }
+
+        public static async Task<MqttClientConnectResult> ConnectWithSasAsync(this IMqttClient mqttClient, string hostName, string deviceId, string moduleId, string sasKey, string modelId = "", int minutes = 60)
+        {
+            (string username, byte[] password) = SasAuth.GenerateHubSasCredentials(hostName, deviceId, moduleId, sasKey, modelId, minutes);
+            return await mqttClient.ConnectAsync(new MqttClientOptionsBuilder()
+                 .WithClientId($"{deviceId}/{moduleId}")
+                 .WithTcpServer(hostName, 8883)
+                 .WithCredentials(username, password)
+                 .WithTls(new MqttClientOptionsBuilderTlsParameters
+                 {
+                     UseTls = true,
+                     SslProtocol = SslProtocols.Tls12
+                 })
+                 .Build());
+        }
+
+        public static async Task<MqttClientConnectResult> ConnectWithX509Async(this IMqttClient mqttClient, string hostName, X509Certificate cert, string modelId = "")
+        {
+            var cid = cert.Subject[3..];
+            string deviceId = cid;
+            string moduleId = string.Empty;
+            
+            if (cid.Contains("/")) // is a module
+            {
+                var segments = cid.Split('/');
+                deviceId = segments[0];
+                moduleId = segments[1];
+            }
+                                 
+            string username = SasAuth.GetUserName(hostName, deviceId, moduleId, string.Empty, modelId, "X509");
+            
+            return await mqttClient.ConnectAsync(
+               new MqttClientOptionsBuilder()
+                   .WithClientId(cid)
+                   .WithTcpServer(hostName, 8883)
+                   .WithCredentials(new MqttClientCredentials()
+                   {
+                       Username = username
+                   })
+                   .WithTls(new MqttClientOptionsBuilderTlsParameters
+                   {
+                       UseTls = true,
+                       SslProtocol = SslProtocols.Tls12,
+                       Certificates = new List<X509Certificate> { cert }
+                   })
+                   .Build(),
+               CancellationToken.None);
+        }
+
+        public static IMqttClient CreateMqttClientWithLogger(TextWriter writer = null)
+        {
+            Trace.Listeners[0].Filter = new EventTypeFilter(SourceLevels.Information);
+            IMqttClient client;
+            if (writer == null)
+            {
+                client = new MqttFactory().CreateMqttClient();
+            }
+            else
+            {
+                Trace.Listeners.Add(new TextWriterTraceListener(writer));
+                Trace.Listeners[1].Filter = new EventTypeFilter(SourceLevels.Information);
+
+                var logger = new MqttNetEventLogger();
+                logger.LogMessagePublished += (s, e) =>
+                {
+                    var trace = $">> [{e.LogMessage.Timestamp:O}] [{e.LogMessage.ThreadId}]: {e.LogMessage.Message}";
+                    if (e.LogMessage.Exception != null)
+                    {
+                        trace += Environment.NewLine + e.LogMessage.Exception.ToString();
+                    }
+
+                    Trace.TraceInformation(trace);
+                };
+                client = new MqttFactory(logger).CreateMqttClient();
+            }
+            return client;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # MQTTnet.Extensions.IoTHubAuth
+
 Extensions to MQTTnet to connect to IoTHub
+
+## Connect to IoTHub 
+
+To estabish a secure MQTT Connection to  Azure IoT Hub MQTT Broker you need to follow the authentication scheme, associated with the **api version** `2021-06-30-preview`. 
+
+> Note: Tat the time of writing only avaialble as a [private preview](https://github.com/Azure/IoTHubMQTTBrokerPreviewSamples#private-preview-program-information)
+
+This project includes extension methods to the `IMqttClient` interface to connect with SasKeys or X509 certificates, for device and module identities.
+
+## How to use
+
+The code is available as a NuGet source package that will add 2 files to your existing project.
+
+```cs
+var client = new MqttFactory().CreateMqttClient();
+var conack = await client.ConnectWithSasAsync("<broker-enabled-hub>","<deviceId","<deviceKey");
+```
+
+### Tracing
+
+Diagnostics logs can be enabled by creating the client with:
+
+```cs
+var client = IMqttClientExtensions.CreateMqttClientWithLogger(Console.Out);
+```
+
+### Thanks to
+
+The csproj to produce NuGet source packages from [this gist](https://gist.github.com/attilah/fd3e71f03fd258c496179e0200c57b0b#file-x-y-z-sources-csproj).
+
+
+
